7a1ecffad3f74eeedc117c2800324f305b75e725
John Bollig <johnbollig@Johns-iMac.local>
2018-04-25 18:37:42 -0600

    Write the inital script

---
+++ 2GigPermSwap.sh
@@ -0,0 +1,36 @@
+#!/bin/sh
+#Version 0.0.0.1
+#Info: Installs 2GB swaw file
+#Testing OS: Ubuntu 16.04
+#TODO: Test this on a server
+#TODO: Check this code on other version of Ubuntu
+
+
+message() {
+	echo "╒═════════════════════════════════════════════════════════════════>>>"
+	echo "| $1"
+	echo "╘═════════════════════════════════════════════════════════════════<<<"
+}
+
+createswap() {
+	message "Creating 2GB permament swap file...this may take a few minutes..."
+	sudo dd if=/dev/zero of=/swapfile bs=1M count=2000
+	sudo mkswap /swapfile
+	sudo chown root:root /swapfile
+	sudo chmod 0600 /swapfile
+	sudo swapon /swapfile
+	sudo chmod 0600 /swapfile
+	sudo chown root:root /swapfile
+	sudo echo "/swapfile none swap sw 0 0" >> /etc/fstab
+  message "2GB swap has been created and the /etc/fstab has been edited"
+}
+
+
+
+install() {
+	createswap
+}
+
+#main
+#default to --without-gui
+install --without-gui
