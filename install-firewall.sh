054eb2a53cc288c7198a7e3442bc99dc23d179d2
J B <37249474+seekthex@users.noreply.github.com>
2018-04-27 10:08:38 -0400

    edit description

--- install-firewall.sh
+++ install-firewall.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 #Version 0.0.0.1
-#Info: Installs 2GB swap file
+#Info: Installs firewall for CHC
 #Tested OS: Clean Install of Ubuntu 16.04 (pass)
 #TODO: Check this code on other version of Ubuntu
 
